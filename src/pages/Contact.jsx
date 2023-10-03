bf7a3245862d2fee60e5bbbae59bd4c229989ea0
Alish Baig <alishbaig2004@gmail.com>
2023-10-03 09:58:38 +0530

    feat: add contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+        <div className="mt-8 mb-4">
+        <h1 className="text-3xl font-extrabold">Contact</h1>
+        <p>I don't know what to put here</p>
+      </div>
+    )
+}
