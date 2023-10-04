abc4eddfaad3585e22becdf2af90a08df27d3e6d
Alish Baig <alishbaig2004@gmail.com>
2023-10-04 22:41:16 +0530

    refactor: move data to data file

---
+++ src/data.js
@@ -0,0 +1,130 @@
+export const projects = [
+    {
+        title:"a1x5h04.github.io",
+        description:"Personal website hosted on github pages",
+        tags:['reactjs','portfolio','pages'],
+        language:{text:"Javascript" , color:"#facc15"},
+        link:"https://a1x5h04.github.io"
+    },
+    {
+        title:"quizzapp",
+        description:"Personal website hosted on github pages",
+        tags:['reactjs','api','javascript', 'figma'],
+        language:{text:"Javascript" , color:"#facc15"},
+        link:"https://a1x5h04.github.io"
+    },
+    {
+        title:"watchflick",
+        description:"A Movie/Anime Streaming App Made With React, Ionic and Tailwind CSS",
+        tags:['ionic','reactjs','watchlist', 'tailwindcss', 'streaming-web'],
+        language:{text:"Javascript", color:"#facc15"},
+        link:"https://watchflick.netlify.app"
+    },
+    {
+        title:"dictionary-app",
+        description:"A beautiful dictionary app made with React and Urban Dictionary API",
+        tags:['web', 'reactjs', 'dictionary', 'urban', 'vitejs'],
+        language:{text:"Javascript" , color:"#facc15"},
+        link:"https://a1x5h04-dictionary.netlify.app/"
+    },
+     
+]
+
+export const languages = [
+    {
+        link: "https://www.javascript.com/",
+        imgAltText: "Javascript",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+        skillName: "Javascript",
+        },
+        {
+        link: "https://www.typescriptlang.org/",
+        imgAltText: "TypeScript",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+        skillName: "TypeScript",
+        },
+        {
+        link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+        imgAltText: "CSS 3",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        skillName: "CSS3",
+        },
+        {
+        link: "https://tailwindcss.com/",
+        imgAltText: "Tailwind CSS",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+        skillName: "Tailwind CSS",
+        },
+        {
+        link: "https://getbootstrap.com/",
+        imgAltText: "Bootstrap",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+        skillName: "Bootstrap",
+        },
+        {
+        link: "https://reactjs.org/",
+        imgAltText: "React JS",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        skillName: "React JS",
+        },
+        {
+            link: "https://docs.microsoft.com/en-us/cpp/?view=msvc-170",
+            imgAltText: "C",
+            imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg",
+            skillName: "C",
+            },
+            {
+            link: "https://docs.microsoft.com/en-us/cpp/?view=msvc-170",
+            imgAltText: "C++",
+            imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+            skillName: "C++",
+            },
+            {
+            link: "https://kotlinlang.org/",
+            imgAltText: "Kotlin",
+            imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg",
+            skillName: "Kotlin",
+            },
+            {
+            link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+            imgAltText: "HTML 5",
+            imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+            skillName: "HTML5",
+            },
+            {
+            link: "https://nodejs.org/en/",
+            imgAltText: "Node.js",
+            imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+            skillName: "Node.js",
+            },
+            {
+            link: "https://www.mysql.com/",
+            imgAltText: "MySQL",
+            imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+            skillName: "MySQL",
+            },
+            {
+            link: "https://firebase.google.com/",
+            imgAltText: "Firebase",
+            imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+            skillName: "Firebase",
+            },
+            {
+            link: "https://www.figma.com/",
+            imgAltText: "Figma",
+            imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+            skillName: "Figma",
+            },
+            {
+            link: "https://git-scm.com/",
+            imgAltText: "Git",
+            imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+            skillName: "Git",
+            },
+            {
+            link: "https://dart.dev",
+            imgAltText: "Dart",
+            imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg",
+            skillName: "Dart",
+        }
+]
