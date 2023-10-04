593f8bab00f46655f5ec86953dd11b010d2de4cc
Alish Baig <alishbaig2004@gmail.com>
2023-10-04 17:12:37 +0530

    style: add padding in loading elm

--- src/components/ImageLoader.jsx
+++ src/components/ImageLoader.jsx
@@ -1,10 +1,10 @@
 import { FileX } from "@phosphor-icons/react";
 import { useState, useEffect } from "react";
 
-const ImageLoader = ({ imageUrl, ...props }  ) => {
+const ImageLoader = ({ imageUrl, ...props }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-    console.log("ImageLoader", imageUrl)
+  console.log("ImageLoader", imageUrl);
   useEffect(() => {
     const img = new Image();
 
@@ -52,11 +52,11 @@ function ErrorElement() {
 
 function LoadingElement() {
   return (
-    <div>
+    <div className="p-10">
       <div role="status">
         <svg
           aria-hidden="true"
-          class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-white dark:fill-gray-600"
+          class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-800 fill-black dark:fill-white"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
