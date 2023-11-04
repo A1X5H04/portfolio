44eef2ba8377719cb2acb923a920f326b4196512
Alish <78891448+A1X5H04@users.noreply.github.com>
2023-11-04 18:48:56 +0530

    fix: map is not a function error

--- src/api.js
+++ src/api.js
@@ -1,29 +1,21 @@
-export async function getPinnedGHRepo() {
+export async function getProjects() {
     try {
-        const res = await fetch('https://gh-pinned-repos.egoist.dev/?username=a1x5h04');
+        const res = await fetch('https://api.npoint.io/8a1ae503884eede2a37f/project_repos');
         const data = await res.json();
-        if(data.length > 0) {
-            return data;
-        } else {
-            const res = await fetch('https://api.npoint.io/8a1ae503884eede2a37f/project_repos');
-            const data = await res.json();
-            return data;
-        }
+        return data;
     } catch (err) {
-        console.error(err);
-        return "Failed to fetch Projects Data"
+        throw new Error("Failed to fetch Projects Data \n" + err)
     }
 }
 
+
 export async function getLanguages() {
     try {
-
         const res = await fetch('https://api.npoint.io/8a1ae503884eede2a37f/languages/');
         const data = await res.json();
         return data;
     } catch (err) {
-        console.error(err);
-        return "Failed to fetch Languages Data"
+        throw new Error("Failed to fetch Languages Data \n" + err)
     }
 }
 
@@ -33,7 +25,21 @@ export async function getAbout() {
         const data = await res.json();
         return data;
     } catch (err) {
-        console.error(err);
-        return "Failed to fetch About Data"
+        throw new Error("Failed to fetch About Data \n" + err)
+    }
+}
+
+
+export async function getPinnedGHRepo() {
+    try {
+        const res = await fetch('https://gh-pinned-repos.egoist.dev/?username=a1x5h04');
+        const data = await res.json();
+        if (data.length > 0) {
+            return data;
+        } else {
+           getProjects();
+        }
+    } catch (err) {
+        throw new Error("Failed to fetch Pinned Github Projects Data \n" + err)
     }
 }
