c35a059d4144ca5510ea71a1c2aa09d4f63578fd
Alish <alishbaig2004@gmail.com>
2024-04-06 10:00:43 +0530

    style: Add Inter Tight font family to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        display: ["Inter Tight", "display", "sans-serif"],
+      },
       colors: {
         "gray-100": "#f8f9fa",
         "gray-200": "#e9ecef",
